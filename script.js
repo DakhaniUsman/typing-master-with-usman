a679fd295189a46142dfbfd5db5a6b8c4d95934f
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-09 16:45:55 +0530

    updates

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ const renderNewQuote = async () => {
     //Access content from JSON
     quote = data.content;
     console.log(quote);
-    let arr = quote.split("").map((value)=>{
+    let arr = quote.split("").map((value) => {
         //wrap the each character in span tag
-        return "<span class='quote-chars'>"  + value +"</span>"
-    });    
+        return "<span class='quote-chars'>" + value + "</span>"
+    });
 
     quoteSection.innerHTML = arr.join("");
     console.log(arr);
 }
 //Logic for comparing input words with quote
 
-userInput.addEventListener("input",()=>{
+userInput.addEventListener("input", () => {
     let quoteChars = document.querySelectorAll(".quote-chars")
 
     //Creating an Array name quoteChars from recieved span tags object
@@ -35,40 +35,40 @@ userInput.addEventListener("input",()=>{
     //User inputs of each character in Textbox
     let userInputChars = userInput.value.split("");
 
-    quoteChars.forEach((char,index)=>{
-        if(char.innerHTML == userInputChars[index]){
+    quoteChars.forEach((char, index) => {
+        if (char.innerHTML == userInputChars[index]) {
             char.classList.add("success");
-        } 
-        else if(userInputChars[index] == null){
-            if(char.classList.contains("fail")){
+        }
+        else if (userInputChars[index] == null) {
+            if (char.classList.contains("fail")) {
                 char.classList.remove("fail")
-            }else{
+            } else {
                 char.classList.remove("success")
             }
         }
-        else{
-            if(!char.classList.contains("fail")){
+        else {
+            if (!char.classList.contains("fail")) {
                 mistakes = mistakes + 1;
                 char.classList.add("fail")
             }
             document.getElementById("mistakesID").innerHTML = mistakes;
-            }
+        }
     });
 });
 
-function updateTimer(){
-    if(time == 0){
+function updateTimer() {
+    if (time == 0) {
         displayResult();
     }
-    else{
+    else {
         document.getElementById("timerID").innerHTML = --time + "s";
     }
 }
-const timeReduce = ()=>{
+const timeReduce = () => {
     time = 60;
-    timer = setInterval(updateTimer,1000);
+    timer = setInterval(updateTimer, 1000);
 }
-const displayResult = () =>{
+const displayResult = () => {
     document.querySelector(".result").style.display = "block";
     clearInterval(timer);
     document.getElementById("stop-test").style.display = "none";
@@ -76,34 +76,34 @@ const displayResult = () =>{
     userInput.disabled = true;
 
     let timeTaken = 1;
-    if(time !=0){
-        timeTaken = (60-time)/100;
-    }    
-    document.getElementById('speedID').innerText = (userInput.value.length/5/timeTaken).toFixed(2) + "wpm";
+    if (time != 0) {
+        timeTaken = (60 - time) / 100;
+    }
+    document.getElementById('speedID').innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + "wpm";
 
-    document.getElementById('accuracyID').innerText = Math.round(((userInput.value.length - mistakes)/ userInput.value.length)*100) + "%";
+    document.getElementById('accuracyID').innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100) + "%";
 }
-const startTest = () =>{
-    timer= "";
+const startTest = () => {
+    timer = "";
     mistakes = 0;
-    userInput.disabled= false;
+    userInput.disabled = false;
     timeReduce();
-    document.getElementById("start-test").style.display="none";
+    document.getElementById("start-test").style.display = "none";
     document.getElementById("stop-test").style.display = "block";
 }
-window.onload = () =>{
+window.onload = () => {
     userInput.value = "";
     document.getElementById("start-test").style.display = "block";
     document.getElementById("stop-test").style.display = "none";
     document.getElementById("restart-test").style.display = "none";
     userInput.disabled = true;
     renderNewQuote();
-    
+
 }
 
 const restartTest = () => {
     window.onload();
-    
+
 
 }
 
